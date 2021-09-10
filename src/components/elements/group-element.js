1471948937428559c639383f0040981936d18abd
jocruz-dev <joshuapedraza@MacBook-de-Mac.local>
2021-09-10 10:47:09 -0500

    fix: Home page

--- src/components/elements/group-element.js
+++ src/components/elements/group-element.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Container, Row, Col, Button } from 'react-bootstrap'
+import {Container, Row, Col, Image } from 'react-bootstrap'
+import edit from '../../static/images/edit.png'
 
 const GroupElement = ({name, status})=>{
     return(
         <Container fluid>
-            <Row fluid className="d-flex flex-row p-2">
-                <Col xs={6}>{name}</Col>
-                <Col>
-                    <Button className="button_send" size="sm" variant="outline_light">Nuevo grupo</Button>
+            <Row fluid className="p-2">
+                <Col xs="8">{name}</Col>
+                <Col xs="2" className="justify-content-end">
+                <Image alt="edit" className="img-fluid" src={edit}/>
                 </Col>
             </Row>
         </Container>
