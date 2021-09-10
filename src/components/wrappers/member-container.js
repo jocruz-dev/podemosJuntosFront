c1709913092cd1ea786e4c8be23755c51b8eca93
jocruz-dev <joshuapedraza@MacBook-de-Mac.local>
2021-09-10 12:45:10 -0500

    feat: api pdf

--- src/components/wrappers/member-container.js
+++ src/components/wrappers/member-container.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useHistory } from "react-router-dom";
 
 const MemberContainer = ()=>{
+    const [data, setData] = useState()
+    let history = useHistory();
+    const handleClick = async ()=>{
+        const response = await fetch('http://portafolio-env.eba-d3j6tvyk.us-east-1.elasticbeanstalk.com/api/score')
+        const responseJson = await response.json()
+        setData(JSON.stringify(responseJson))
+    }
+
     return (
-        <h1>Member</h1>
+        <div>
+            <h1>Member</h1>
+            <button ><a href="http://portafolio-env.eba-d3j6tvyk.us-east-1.elasticbeanstalk.com/api/pdf">Get PDF</a></button>
+            <button onClick={handleClick}>Get DATA</button>
+            <p>{data}</p>
+        </div>
     )
 }
 
