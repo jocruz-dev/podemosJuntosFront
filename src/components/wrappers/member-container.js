f77317d4529362da9917a4f4c21a66c085da2187
jocruz-dev <joshuapedraza@MacBook-de-Mac.local>
2021-09-10 12:59:03 -0500

    feat: api pdf

--- src/components/wrappers/member-container.js
+++ src/components/wrappers/member-container.js
@@ -5,7 +5,7 @@ const MemberContainer = ()=>{
     const [data, setData] = useState()
     
     const handleClick = async ()=>{
-        const response = await fetch('https://portafolio-env.eba-d3j6tvyk.us-east-1.elasticbeanstalk.com/api/score')
+        const response = await fetch('http://portafolio-env.eba-d3j6tvyk.us-east-1.elasticbeanstalk.com/api/score')
         const responseJson = await response.json()
         setData(JSON.stringify(responseJson))
     }
@@ -13,7 +13,7 @@ const MemberContainer = ()=>{
     return (
         <div>
             <h1>Member</h1>
-            <button ><a href="https://portafolio-env.eba-d3j6tvyk.us-east-1.elasticbeanstalk.com/api/pdf">Get PDF</a></button>
+            <button ><a href="http://portafolio-env.eba-d3j6tvyk.us-east-1.elasticbeanstalk.com/api/pdf">Get PDF</a></button>
             <button onClick={handleClick}>Get DATA</button>
             <p>{data}</p>
         </div>
